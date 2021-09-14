33104d657db087aa6f346bfb1689c765433e491a
pvelp <72802588+pvelp@users.noreply.github.com>
2021-09-14 15:22:39 +0300

    second commit

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,52 @@
-#include <header.hpp>
+#include <student.hpp>
+#include <fstream>
 
-int main() {
+using nlohmann::json;
 
+
+void print(const Student& student, std::ostream& os) {
+    //...
+    if (student.debt.type() == typeid(std::nullptr_t)) {
+        os << "null";
+    } else if (student.debt.type() == typeid(std::string)) {
+        os << std::any_cast<std::string>(student.debt);
+    } else {
+        os
+                << std::any_cast<std::vector<std::string> >(student.debt).size()
+                << " items";
+    }
+}
+
+void print(const std::vector<Student>& students, std::ostream& os) {
+
+    //...
+    for (auto const& student : students) {
+        print(student, os);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::string jsonPath;
+    if (argc < 1){
+        std::cout << "Не передали путь к файлу" << std::endl;
+    } else {
+        jsonPath = argv[1];
+    }
+//...
+    std::ifstream file{jsonPath};
+    if (!file) {
+        throw std::runtime_error{"unable to open json: " + jsonPath};
+    }
+
+    json data;
+    file >> data;
+
+    std::vector<Student> students;
+    for (auto const& item : data.at("items")) {
+        Student student;
+        from_json(item, student);
+        students.push_back(student);
+    }
+    //...
+    print(students, std::cout);
 }
--- include/student.hpp
+++ include/student.hpp
@@ -2,5 +2,30 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <iostream>
+#include <any>
+#include <nlohmann/json.hpp>
+
+
+using nlohmann::json;
+
+struct Student{
+    std::string name;
+    std::any group;
+    std::any avg;
+    std::any debt;
+};
+
+auto get_name(const json& j) -> std::string;
+
+auto get_debt(const json& j) -> std::any;
+
+auto get_avg(const json& j) -> std::any;
+
+auto get_group(const json& j) -> std::any;
+
+void from_json(const json& j, Student& s);
+
+
 
 #endif // INCLUDE_HEADER_HPP_
